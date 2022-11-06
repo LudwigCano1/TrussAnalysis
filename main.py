6a4cf7c6eb71c100137e18877d606ab1ee9e588f
LudwigCano1 <115447154+LudwigCano1@users.noreply.github.com>
2022-11-05 21:18:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from pydoc import visiblename
 import streamlit as st
 import plotly_express as px
 from functions import Assemble_K_element, Assemble_K_general,Fix,load
 from numpy.linalg import solve,det
 from numpy import array,zeros
+from math import sqrt
 
 st.set_page_config(page_title="Truss Analysis",page_icon=":shark:")
 
@@ -15,16 +15,16 @@ with st.sidebar:
     st.write("Temperature changes are not considered.")
     st.write("Fabrication error of elements are not considered.")
     #st.markdown("[aea](https://www.google.com)")
-    st.write("Colors:")
-    c1,c2 = st.columns([3,1])
-    with c1: st.write("Node color:")
-    with c2: col_joint = st.color_picker("col_j",label_visibility="collapsed")
-    c1,c2 = st.columns([3,1])
-    with c1: st.write("Element color:")
-    with c2: col_elem = st.color_picker("col_e",label_visibility="collapsed")
-    c1,c2 = st.columns([3,1])
-    with c1: st.write("Constraint color:")
-    with c2: col_const = st.color_picker("col_c",label_visibility="collapsed")
+    with st.expander("Colors:"):
+        c1,c2 = st.columns([3,1])
+        with c1: st.write("Node color:")
+        with c2: col_joint = st.color_picker("col_j","#FD0606",label_visibility="collapsed")
+        c1,c2 = st.columns([3,1])
+        with c1: st.write("Element color:")
+        with c2: col_elem = st.color_picker("col_e","#07F7C0",label_visibility="collapsed")
+        c1,c2 = st.columns([3,1])
+        with c1: st.write("Constraint color:")
+        with c2: col_const = st.color_picker("col_c","#1200FF",label_visibility="collapsed")
 
 with Tab1:
     c1,c2,c3 = st.columns(3,gap="large")
@@ -120,14 +120,31 @@ with Tab2:
     st.write("---")
     st.plotly_chart(plot1,use_container_width=True)
 
+x_max = Nudos_Usados["Joint #01"][0]
+x_min = Nudos_Usados["Joint #01"][0]
+y_max = Nudos_Usados["Joint #01"][1]
+y_min = Nudos_Usados["Joint #01"][1]
+
+for nudo in Nudos_Usados:
+    if Nudos_Usados[nudo][0] < x_min:
+        x_min = Nudos_Usados[nudo][0]
+    if Nudos_Usados[nudo][0] > x_max:
+        x_max = Nudos_Usados[nudo][0]
+    if Nudos_Usados[nudo][1] < y_min:
+        y_min = Nudos_Usados[nudo][1]
+    if Nudos_Usados[nudo][1] > y_max:
+        y_max = Nudos_Usados[nudo][1]
+
+dist_max = max(x_max-x_min,y_max-y_min)/10
+
 with Tab3:
     st.write("Active the checkbox to restraint the displacement in that direction")
     c1,c2,c3,c4,c5 = st.columns([2,1,1,1,2])
     label = " "
     dict_constraints = {}
     with c2: st.write("**Joint**")
-    with c3: st.write("**DX**")
-    with c4: st.write("**DY**")
+    with c3: st.write("**RX**")
+    with c4: st.write("**RY**")
     for i in Nudos_Usados:
         with c2: st.write(i)
         with c3:
@@ -139,11 +156,11 @@ with Tab3:
         if DX or DY:
             dict_constraints[i] = [int(DX),int(DY),Nudos_Usados[i][2],Nudos_Usados[i][3]]
             if DX and not DY:
-                plot1.add_scatter(x=[Nudos_Usados[i][0]],y=[Nudos_Usados[i][1]],marker={"symbol":"8","size":25,"color":col_const})
+                plot1.add_scatter(x=[Nudos_Usados[i][0]],y=[Nudos_Usados[i][1]],marker={"symbol":"8","size":30,"color":col_const})
             if DY and not DX:
-                plot1.add_scatter(x=[Nudos_Usados[i][0]],y=[Nudos_Usados[i][1]],marker={"symbol":"5","size":25,"color":col_const})
+                plot1.add_scatter(x=[Nudos_Usados[i][0]],y=[Nudos_Usados[i][1]],marker={"symbol":"5","size":30,"color":col_const})
             if DY and DX:
-                plot1.add_scatter(x=[Nudos_Usados[i][0]],y=[Nudos_Usados[i][1]],marker={"symbol":"1","size":25,"color":col_const})
+                plot1.add_scatter(x=[Nudos_Usados[i][0]],y=[Nudos_Usados[i][1]],marker={"symbol":"1","size":20,"color":col_const})
     st.plotly_chart(plot1,use_container_width=True)
 
 with Tab4:
@@ -171,7 +188,7 @@ F = load(F,list_forces)
 st.write("---")
 
 with Tab5:
-    pes1,pes2 = st.tabs(["[ General ]","[ Elements ]"])
+    pes1,pes2,pes3 = st.tabs(["[ General ]","[ Elements ]","[ Deformed ]"])
     with pes1:
         c1,c2,c3 = st.columns([3,1,1])
         if det(K) != 0:
@@ -214,3 +231,43 @@ with Tab5:
                 st.write(F_el)
         else:
             st.write("The constraints are not enough for equilibrium")
+        Joint_i = dict_Elem[ver_elem][0]
+        Joint_f = dict_Elem[ver_elem][1]
+        delta_x = -dict_Nudos[Joint_i][0]+dict_Nudos[Joint_f][0]
+        delta_y = -dict_Nudos[Joint_i][1]+dict_Nudos[Joint_f][1]
+        L = sqrt(delta_x**2+delta_y**2)
+        delta_u = U[Nudos_Usados[Joint_f][2]][0] - U[Nudos_Usados[Joint_i][2]][0]
+        delta_v = U[Nudos_Usados[Joint_f][3]][0] - U[Nudos_Usados[Joint_i][3]][0]
+        N = dict_Elem[ver_elem][2]*dict_Elem[ver_elem][3]*(delta_x*delta_u+delta_y*delta_v)/(L**2)
+        if N > 0: sss = "T"
+        elif N < 0: sss = "C"
+        else: sss = "0"
+        st.write(f"Axial Force: {N:.2f} {force_unit} **{sss}**")
+
+disp_max = max(abs(U))[0]
+
+Nudos_Desplazados = {}
+for n in Nudos_Usados:
+    Nudos_Desplazados[n] = Nudos_Usados[n]
+    Nudos_Desplazados[n][0] = Nudos_Usados[n][0] + U[Nudos_Usados[n][2]][0]*dist_max/disp_max
+    Nudos_Desplazados[n][1] = Nudos_Usados[n][1] + U[Nudos_Usados[n][3]][0]*dist_max/disp_max
+
+with pes3:
+    plot_deformed = px.line(x=[0],y=[0],labels={"x":f"x ({length_unit})","y":f"y ({length_unit})"},range_x=[x_min-dist_max*1.5,x_max+dist_max*1.5],range_y=[y_min-dist_max*1.5,y_max+dist_max*1.5])
+    plot_deformed.update_layout(showlegend=False)
+    for el in dict_Elem:
+        Joint_i = dict_Elem[el][0]
+        Joint_f = dict_Elem[el][1]
+        delta_x = -dict_Nudos[Joint_i][0]+dict_Nudos[Joint_f][0]
+        delta_y = -dict_Nudos[Joint_i][1]+dict_Nudos[Joint_f][1]
+        L = sqrt(delta_x**2+delta_y**2)
+        delta_u = U[Nudos_Usados[Joint_f][2]][0] - U[Nudos_Usados[Joint_i][2]][0]
+        delta_v = U[Nudos_Usados[Joint_f][3]][0] - U[Nudos_Usados[Joint_i][3]][0]
+        N = dict_Elem[el][2]*dict_Elem[el][3]*(delta_x*delta_u+delta_y*delta_v)/(L**2)
+        if N < 0: color = "#FF0000"
+        elif N > 0: color = "#0000FF"
+        else: color = "#FFFF00"
+        plot_deformed.add_scatter(x=[dict_Nudos[Joint_i][0],dict_Nudos[Joint_f][0]],y=[dict_Nudos[Joint_i][1],dict_Nudos[Joint_f][1]],mode="lines",line={"color":"#000000","width":0.5,"dash":"dash"})
+        plot_deformed.add_scatter(x=[Nudos_Desplazados[Joint_i][0],Nudos_Desplazados[Joint_f][0]],y=[Nudos_Desplazados[Joint_i][1],Nudos_Desplazados[Joint_f][1]],mode="lines",line={"color":color,"width":4})
+        plot_deformed.add_scatter(x=[0.5*(Nudos_Desplazados[Joint_i][0]+Nudos_Desplazados[Joint_f][0])],y=[0.5*(Nudos_Desplazados[Joint_i][1]+Nudos_Desplazados[Joint_f][1])],mode="lines+text",line={"color":col_elem,"width":4},text=f"{N:.3f}",textposition="bottom center",textfont={"size":18,"color":color})
+    st.plotly_chart(plot_deformed,use_container_width=True)
